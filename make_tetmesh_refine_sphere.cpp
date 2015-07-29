20549f1f98605cb758fdd99ca2b5043385fefa6f
Hamid Ghadyani <hamid@hamid.cc>
2015-07-29 03:03:38 -0400

    Clean up source code.

--- make_tetmesh_refine_sphere.cpp
+++ make_tetmesh_refine_sphere.cpp
@@ -1,5 +1,6 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-//#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+
+// #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 #include <CGAL/Mesh_3/Robust_intersection_traits_3.h>
 
 #include <CGAL/Mesh_triangulation_3.h>
@@ -16,9 +17,9 @@
 #include "vector.h"
 
 // Computational Kernel Definitions necessary for robust meshing
-//typedef CGAL::Exact_predicates_exact_constructions_kernel K;
-//typedef CGAL::Polyhedron_3<K>      Polyhedron;
-//typedef CGAL::Polyhedral_mesh_domain_3<Polyhedron> Mesh_domain;
+// typedef CGAL::Exact_predicates_exact_constructions_kernel K;
+// typedef CGAL::Polyhedron_3<K>      Polyhedron;
+// typedef CGAL::Polyhedral_mesh_domain_3<Polyhedron> Mesh_domain;
 
 // Domain
 // typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
@@ -47,7 +48,7 @@ typedef Mesh_criteria::Cell_criteria Cell_criteria;
 // Sizing field
 struct Spherical_sizing_field
 {
-    typedef ::FT FT;
+    typedef::FT FT;
     typedef Point Point_3;
     typedef Mesh_domain::Index Index;
     double x, y, z;
@@ -55,94 +56,98 @@ struct Spherical_sizing_field
 
     FT operator()(const Point_3& p, const int, const Index&) const
     {
-        FT sq_d = CGAL::squared_distance(p, Point(x,y,z));
+        FT sq_d = CGAL::squared_distance(p, Point(x, y, z));
+
         sq_d = CGAL::sqrt(sq_d);
-        if (sq_d <= r + 0.01)
-			return ref_ratio * def_size;
-        else
+
+        if (sq_d <= r + 0.01) {
+            return ref_ratio * def_size;
+        } else {
             return def_size;
-    // FT sq_d_to_origin = CGAL::squared_distance(p, Point(CGAL::ORIGIN));
-    // return CGAL::abs( CGAL::sqrt(sq_d_to_origin)-0.5 ) / 10. + 0.025; 
+        }
+
+        // FT sq_d_to_origin = CGAL::squared_distance(p, Point(CGAL::ORIGIN));
+        // return CGAL::abs( CGAL::sqrt(sq_d_to_origin)-0.5 ) / 10. + 0.025;
     }
 };
 
 struct Spherical_sizing_field2
 {
-    typedef ::FT FT;
+    typedef::FT FT;
     typedef Point Point_3;
     typedef Mesh_domain::Index Index;
-//  double x, y, z;
+
+    //  double x, y, z;
     double ref_ratio, def_size;
     double R1, R2, S1, S2;
-    
+
     double A[3], B[3];
-	double v2[3];
-	double AB, m1, m2;
-	
+    double v2[3];
+    double AB, m1, m2;
+
     FT operator()(const Point_3& p, const int, const Index&) const
     {
         // FT sq_d = CGAL::squared_distance(p, Point(x,y,z));
         // sq_d = CGAL::sqrt(sq_d);
         double P[3];
-		double v1[3];
+        double v1[3];
+
         P[0] = p.x(); P[1] = p.y(); P[2] = p.z();
         v_make(A, P, 3, v1); // v1 = P-A
         // v_make(A, B, 3, v2); // v2 = B-A
-        
-        double k = v_dot(v1, v2, 3) / AB / AB;
-        double pp[3] = {k*v2[0]+A[0], k*v2[1]+A[1], k*v2[2]+A[2]};
+
+        double k     = v_dot(v1, v2, 3) / AB / AB;
+        double pp[3] = { k *v2[0] + A[0], k *v2[1] + A[1], k *v2[2] + A[2] };
 
         double pp_dist = v_dist(pp, P, 3);
-        double localr = m1 * k * AB + R2;
+        double localr  = m1 * k * AB + R2;
 
         double localsize = m2 * k * AB + S2;
+
         // Check if point P lies within the 'cone'
         if (pp_dist <= localr) {
             return localsize;
+        } else {
+            return S2; // fabs(pp_dist - localr)/localr
         }
-        else {
-            return S2; //fabs(pp_dist - localr)/localr 
-        }
-	}
+    }
 };
-
 
 
 using namespace CGAL::parameters;
 
 int main(int argc, char *argv[]) {
     Polyhedron cutbrain;
-	std::string ifn, cfn, outfn;
-    double facet_angle=25, facet_size=3.0, facet_distance=0.5,
-           cell_radius_edge=3,cell_size=3.0;
-    double xa = 0, ya = 0, za = 0, r1 = 1., ref_ratio = 0.3;
-    double xb = 0, yb = 0, zb = 0, r2 = 1.;
-    double s1 = 1., s2 = 1.;
-    
+    std::string ifn, cfn, outfn;
+    double facet_angle      = 25, facet_size = 3.0, facet_distance = 0.5,
+           cell_radius_edge = 3, cell_size = 3.0;
+    double xa               = 0, ya = 0, za = 0, r1 = 1., ref_ratio = 0.3;
+    double xb               = 0, yb = 0, zb = 0, r2 = 1.;
+    double s1               = 1., s2 = 1.;
+
     bool defulatcriteria = false;
 
     if (argc == 1) {
         std::cout << " Enter the surface mesh filename: ";
         std::cin >> ifn;
-		std::cout << " Enter the output mesh file name: ";
-		std::cin >> outfn;
+        std::cout << " Enter the output mesh file name: ";
+        std::cin >> outfn;
         defulatcriteria = true;
         std::cout << " (Using default settings for meshing parameters!)\n";
-    }
-    else if (argc==3) {
-        ifn = argv[1];
-		outfn = argv[2];
+    } else if (argc == 3) {
+        ifn             = argv[1];
+        outfn           = argv[2];
         defulatcriteria = true;
-    }
-    else if (argc == 4) {
-        ifn = argv[1];
-		outfn = argv[2];
-        cfn = argv[3];
+    } else if (argc == 4) {
+        ifn   = argv[1];
+        outfn = argv[2];
+        cfn   = argv[3];
     }
 
     if (!defulatcriteria) {
         std::cout << " Reading criteria from file." << std::endl;
         std::ifstream cfs(cfn.c_str());
+
         if (!cfs) {
             std::cerr << " Can not read mesh criteria file!\n";
             exit(-1);
@@ -160,12 +165,13 @@ int main(int argc, char *argv[]) {
         cfs >> zb;
         cfs >> r1; // radius on tumor side
         cfs >> r2; // radius on brain side
-        cfs >> s1; // tet size on tumor side 
+        cfs >> s1; // tet size on tumor side
         cfs >> s2; // tet size on brain side
         cfs >> ref_ratio;
     }
-    
+
     std::ifstream ifs(ifn.c_str());
+
     if (!ifs) {
         std::cerr << " Could not open input surface mesh file!" << std::endl;
         exit(EXIT_FAILURE);
@@ -173,57 +179,64 @@ int main(int argc, char *argv[]) {
 
     std::cout << "    cgal_mesh: reading the input polyhedron.\n";
     ifs >> cutbrain;
-//    std::cout << "    cgal_mesh: done reading the input polyhedron.\n";
-    
+
+    //    std::cout << "    cgal_mesh: done reading the input polyhedron.\n";
+
     if (!cutbrain.is_closed()) {
         std::cerr << " Input polyhedron is not closed!\n";
         exit(-1);
     }
-    
+
     Mesh_domain domain(cutbrain);
 
-//    std::cout << "    cgal_mesh: setting domain criteria.\n";
-    
+    //    std::cout << "    cgal_mesh: setting domain criteria.\n";
+
     Spherical_sizing_field2 size;
-    
+
     size.ref_ratio = ref_ratio;
-    size.B[0] = xb; size.B[1] = yb; size.B[2] = zb;
-    size.A[0] = xa; size.A[1] = ya; size.A[2] = za;
-    size.def_size = cell_size;
-    size.R1 = r1; size.R2 = r2;
-    size.S1 = s1; size.S2 = s2;
+    size.B[0]      = xb; size.B[1] = yb; size.B[2] = zb;
+    size.A[0]      = xa; size.A[1] = ya; size.A[2] = za;
+    size.def_size  = cell_size;
+    size.R1        = r1; size.R2 = r2;
+    size.S1        = s1; size.S2 = s2;
     v_make(size.A, size.B, 3, size.v2);
-	size.AB = v_magn(size.v2, 3);
-	size.m1 = (size.R1 - size.R2) / size.AB;
-	size.m2 = (size.S1 - size.S2) / size.AB;
-	
+    size.AB = v_magn(size.v2, 3);
+    size.m1 = (size.R1 - size.R2) / size.AB;
+    size.m2 = (size.S1 - size.S2) / size.AB;
+
     std::cout << "facet_angle " << facet_angle << ' ' << std::endl;
     std::cout << "facet_size " << facet_size << ' ' << std::endl;
     std::cout << "facet_distance " << facet_distance << ' ' << std::endl;
     std::cout << "cell_radius_edge " << cell_radius_edge << ' ' << std::endl;
     std::cout << "cell_size " << cell_size << ' ' << std::endl;
     std::cout << "A: " << xa << ' ' << ya << ' ' << za << std::endl <<
-                 "B: " << xb << ' ' << yb << ' ' << zb << std::endl <<
-                 "R1: " << r1 << std::endl <<
-                 "R2: " << r2 << std::endl <<
-                 "S1: " << s1 << std::endl <<
-                 "S2: " << s2 << std::endl << 
-                 "ref_ratio: " << ref_ratio << std::endl;
-                 
+        "B: " << xb << ' ' << yb << ' ' << zb << std::endl <<
+        "R1: " << r1 << std::endl <<
+        "R2: " << r2 << std::endl <<
+        "S1: " << s1 << std::endl <<
+        "S2: " << s2 << std::endl <<
+        "ref_ratio: " << ref_ratio << std::endl;
+
     // Mesh criteria (no cell_size set)
-    Facet_criteria facet_criteria(facet_angle, facet_size, facet_distance); // angle, size, approximation
-    Cell_criteria cell_criteria(cell_radius_edge, size); // radius-edge ratio, size
+    // angle, size, approximation
+    Facet_criteria facet_criteria(facet_angle, facet_size, facet_distance);
+    
+    // radius-edge ratio, size
+    Cell_criteria cell_criteria(cell_radius_edge, size);
+    
     Mesh_criteria criteria(facet_criteria, cell_criteria);
-    // Mesh_criteria criteria(facet_angle, facet_size, facet_distance, cell_radius_edge, cell_size);
+
+    // Mesh_criteria criteria(facet_angle, facet_size, facet_distance,
+    // cell_radius_edge, cell_size);
 
     std::cout << "    cgal_mesh: running make_mesh_3.\n";
-    C3t3 c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria, no_perturb(), no_exude());
+    C3t3 c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria,
+                                        no_perturb(), no_exude());
 
     std::ofstream medit_file(outfn.c_str());
     c3t3.output_to_medit(medit_file);
     medit_file.close();
-  
+
     return EXIT_SUCCESS;
-    
 }
 
